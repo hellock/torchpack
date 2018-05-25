08ec41dd6ff9fe6890dc95576c1923d0b68aad60
Kai Chen <chenkaidev@gmail.com>
2018-05-25 15:49:26 +0800

    fix import error

--- torchpack/parallel.py
+++ torchpack/parallel.py
@@ -1,7 +1,6 @@
 import multiprocessing
 
 import torch
-import cvbase as cvb
 
 from .io import load_checkpoint
 
@@ -46,6 +45,7 @@ def parallel_test(model_cls,
         idx_queue.put(i)
 
     results = [None for _ in range(len(dataset))]
+    import cvbase as cvb
     prog_bar = cvb.ProgressBar(task_num=len(dataset))
     for _ in range(len(dataset)):
         idx, res = result_queue.get()
